7b00a5caaf799d90b90a374d300a2ef402e2052f
Pavel <PavLego2000@yandex.ru>
2023-04-15 07:32:18 +0300

    Methods "update" (Put) and "getById" (Get) in Controllers

--- src/main/java/com/example/ToDoList/Controllers/TaskController.java
+++ src/main/java/com/example/ToDoList/Controllers/TaskController.java
@@ -1,11 +1,15 @@
 package com.example.ToDoList.Controllers;
 
 import com.example.ToDoList.Models.Task;
+import com.example.ToDoList.Models.User;
 import com.example.ToDoList.Services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/task")
@@ -14,7 +18,7 @@ public class TaskController {
     private TaskService taskService;
 
     @PostMapping("/")
-    public void save(Task task){
+    public void save(@RequestBody Task task){
         taskService.saveTask(task);
     }
 
@@ -27,4 +31,29 @@ public class TaskController {
     public void delete (@PathVariable Integer id) {
         taskService.deleteTaskById(id);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable Integer id){
+        try {
+            Task task = taskService.getTaskById(id);
+            return new ResponseEntity<Task>(task, HttpStatus.OK);
+        }
+        catch (NoSuchElementException e){
+            return new ResponseEntity<Task>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody Task task, @PathVariable Integer id){
+        try {
+            Task new_task = taskService.getTaskById(id);
+            new_task.updateTask(task);
+            taskService.saveTask(new_task);
+            return new ResponseEntity<>(new_task, HttpStatus.OK);
+        }
+        catch (NoSuchElementException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
